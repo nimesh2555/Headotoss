e2bcc5627dbeebf5305c346decc466c69c844681
nimesh2555 <nimesh5@csu.fullerton.edu>
2016-03-20 23:04:14 -0700

    First  commit

---
+++ assignment 5/app.js
@@ -0,0 +1,72 @@
+$(document).ready(function fetchGameResult(data) {
+
+
+
+    $("#1").on("click", function(event) {
+        console.log("1");
+
+        $.ajax({
+            url: "http://localhost:3000/Flip",
+            type: "POST",
+            dataType: "json",
+            contentType: "Application/Json",
+            data: JSON.stringify({
+                "call": "head_s"
+            }),
+            success: function(data) {
+                console.log(data.result);
+                alert("Result :" + data.result);
+            },
+            failure: function(errMsg) {
+                alert(errMsg);
+            }
+        });
+
+
+    });
+    $("#2").on("click", function(event) {
+        console.log("2");
+
+        $.ajax({
+            url: "http://localhost:3000/Flip",
+            type: "POST",
+            dataType: "json",
+            contentType: "Application/Json",
+            data: JSON.stringify({
+                "call": "tail_s"
+            }),
+            success: function(data) {
+                console.log(data.result);
+                alert("Result :" + data.result);
+            },
+            failure: function(errMsg) {
+                alert(errMsg);
+            }
+        });
+
+
+    });
+    $("#3").on("click", function(event) {
+        console.log("3");
+
+        $.ajax({
+            url: "http://localhost:3000/stats",
+            type: "GET",
+            dataType: "json",
+            contentType: "Application/Json",
+            success: function(data) {
+                console.log(data.wins);
+                console.log(data.losses);
+                alert("Wins :" + data.wins + "Losses" + data.losses);
+            },
+            failure: function(errMsg) {
+                alert(errMsg);
+            }
+        });
+
+
+    });
+
+
+
+});
---
+++ assignment 5/server.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var bodyParser = require('body-Parser');
+var app = express();
+var w = 0;
+var l = 0;
+
+
+app.use(express.static('.'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/" + "input1.html");
+});
+
+app.get('/stats', function(req, res) {
+    res.send(JSON.stringify({
+        "wins": w,
+        "losses": l
+    }));
+});
+
+app.use(bodyParser.json());
+app.post('/Flip', function(request, response) {
+    console.log(request.body.call);
+    var x = (Math.random() * 1) + 0;
+    console.log(x);
+    if (x >= 0.5) {
+        if ((request.body.call == "head_s")) {
+            console.log("win head");
+            response.send(JSON.stringify({
+                "result": "win"
+            }));
+            w = w + 1;
+            console.log(w);
+        } else {
+            console.log("loss head");
+            response.send(JSON.stringify({
+                "result": "loss"
+            }));
+            l = l + 1;
+            console.log(l);
+        }
+    } else {
+        if ((request.body.call == "tail_s")) {
+            response.send(JSON.stringify({
+                "result": "win"
+            }));
+            console.log(" win tail");
+            w = w + 1;
+            console.log(w);
+        } else {
+            console.log("loss tail");
+            response.send(JSON.stringify({
+                "result": "loss"
+            }));
+            l = l + 1;
+            console.log(l);
+        }
+    }
+
+});
+
+
+
+app.listen(3000);
+
+
+
